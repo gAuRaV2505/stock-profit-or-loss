21a5434db59eedc1ec79f087d7e9f3836552021b
gAuRaV2505 <asudanigaurav2@gmail.com>
2023-10-17 00:19:11 +0530

    feat: profit or loss calculator
    A vanilla Js app which tells you whether you have made profit or loss on stocks you bought.

---
+++ app.js
@@ -0,0 +1,32 @@
+var buyPrice = document.getElementById('buy-price');
+var stocksQuantity = document.getElementById('quantity');
+var currentPrice = document.getElementById('current-price');
+var submitBtn = document.getElementById('submit-btn');
+var output = document.getElementById('output');
+var container = document.querySelector('.container');
+
+function calculateProfitOrLoss(initial, quantity, current) {
+     var difference= quantity * (current - initial);
+     var diffPercentage = (difference / initial) * 100;
+     
+     if(difference > 0) {
+        output.innerText = `Hey the profit is ${difference} and percent is ${diffPercentage} %`;
+        container.className='profit';
+    } else if(difference < 0) {
+        output.innerText = `Hey the loss is ${-1*difference} and percent is ${-1*diffPercentage} %`;
+        container.className='loss';
+    } else {
+        output.innerText = `You have not made any profit or loss`;
+     }
+}
+
+function submitHandler() {
+    var ip = Number(buyPrice.value);
+    var sq = Number(stocksQuantity.value);
+    var cp = Number(currentPrice.value);
+
+    calculateProfitOrLoss(ip, sq, cp);
+}
+
+submitBtn.addEventListener('click', submitHandler);
+
